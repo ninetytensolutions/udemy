c66e8e65687b4d1226ee142673c159356dcd71dd
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-18 21:40:25 +0000

    added new articles

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,2 +1,11 @@
 class NinetyTenBooking < ActiveRecord::Base
+    
+    validates :date, presence: true
+    validates :time, presence: true
+    validates :firstName, presence: true
+    validates :lastName, presence: true
+    validates :phoneNumber, presence: true { with: /((\w+\s?-?\w+)(,|\z))/i, message: "Please enter in a mobile number"
+    validates :noOfGuests, presence: true
+    validates :username, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :ninety_ten_bookings
   resources :ninety_ten_bookings
   resources :bookings
+  resource :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
