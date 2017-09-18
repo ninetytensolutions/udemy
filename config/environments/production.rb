df668ec8210df847fa604d43d873010d24b2aa04
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-18 10:12:05 +0000

    180917 2010

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.serve_static_assets = true
-  
   config.assets.paths << Rails.root.join("css", "images", "javascripts", "js","fonts","stylesheets")
 end
