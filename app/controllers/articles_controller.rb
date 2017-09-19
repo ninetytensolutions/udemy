227d206d3b50b3c3a00f0524542920f8166b71ce
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-19 00:34:35 +0000

    Added Edit and Update pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,18 +6,42 @@ class ArticlesController < ApplicationController
        
    end
    
-   def show
-       
-   end
-   
    def create
        #render plain: params[:article].inspect
+       
        @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
+       
+        if @article.save
+            flash[:notice] = "Article successfully saved"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
        
    end
    
+   def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            flash[:notice] = "Article successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    
+   
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
    private 
    
         def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :ninety_ten_bookings
   resources :ninety_ten_bookings
   resources :bookings
-  resource :articles
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
