99070bae9d034821b4fc368d206a37a7f8bf1112
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 12:47:33 +0000

    Added Salesforce connect

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,5 +1,12 @@
-class NinetyTenBooking < ActiveRecord::Base
-    
+class NinetyTenBooking < SalesforceModel
+
+
+  self.table_name =  ENV['HEROKUCONNECT_SCHEMA'] + '.booking__c'
+
+  has_many :contacts, :primary_key => "sfid", :foreign_key => "HerokuID"
+
+  attr_protected :CreatedDate, :SystemModstamp, :LastModifiedDate
+
     validates :date, presence: true
     validates :time, presence: true
     validates :firstName, presence: true
