3e04bdb71b7962436f16fd2c5c9c9476f9a3a16e
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 13:42:39 +0000

    1

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -8,7 +8,7 @@
 #    validates :noOfGuests, presence: true
 #    validates :username, presence: true
 # app/models/salesforce/customer.rb
-class Salesforce::Customer < Base
+class Salesforce::NinetyTenBooking < Base
   # this will attempt to connect to salesforce_customers table in the Heroku Connect database
   # modify the expected table name if required
   self.table_name = 'ninetytenbookings.booking__c'
