0ce37bb7ecd442d34c83e506a2255a1a460d902d
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 13:49:07 +0000

    1

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -7,14 +7,8 @@
 #    validates :phoneNumber, presence: true
 #    validates :noOfGuests, presence: true
 #    validates :username, presence: true
-# app/models/salesforce/customer.rb
-class Salesforce::NinetyTenBooking < Base
-  # this will attempt to connect to salesforce_customers table in the Heroku Connect database
-  # modify the expected table name if required
+
+
+class NinetyTenBooking < SalesforceModel
   self.table_name = 'ninetytenbookings.booking__c'
-
-  # ActiveRecord scopes, validations and associations
-  # any other methods, overrides from base class etc ...
-end
-
 end
