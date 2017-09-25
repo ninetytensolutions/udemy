0770c7818fec5f03b2251852128084031422efea
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 13:19:11 +0000

    1

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,4 +1,11 @@
-class NinetyTenBooking < SalesforceModel
-  self.table_name = "ninetytenbookings.booking__c"
+class NinetyTenBooking < ActiveRecord::Base
+    
+    validates :date, presence: true
+    validates :time, presence: true
+    validates :firstName, presence: true
+    validates :lastName, presence: true
+    validates :phoneNumber, presence: true
+    validates :noOfGuests, presence: true
+    validates :username, presence: true
 
 end
