01964926a9f6473a9d0d2273b2c10771a64665af
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 12:53:21 +0000

    Added Salesforce connect

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,13 +1,4 @@
-class NinetyTenBooking < SalesforceModel
-
+class NinetyTenBooking < ActiveRecord::Base
     self.table_name =  'booking__c'
-
-    validates :date, presence: true
-    validates :time, presence: true
-    validates :firstName, presence: true
-    validates :lastName, presence: true
-    validates :phoneNumber, presence: true
-    validates :noOfGuests, presence: true
-    validates :username, presence: true
-
+    
 end
