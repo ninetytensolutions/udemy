11624abd9eeb0b4eac7efbf246c920efe264ec76
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 14:44:40 +0000

    Integrated with Salesforce

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,10 +1,10 @@
 class NinetyTenBooking < ActiveRecord::Base
-    #validates :date__c, presence: true
-    #validates :time__c, presence: true
-    #validates :firstname__c, presence: true
-    #validates :lastname__c, presence: true
-    #validates :phonenumber__c, presence: true
-    #validates :guests__c, presence: true
-    #validates :username__c, presence: true
+    validates :date__c, presence: true
+    validates :time__c, presence: true
+    validates :firstname__c, presence: true
+    validates :lastname__c, presence: true
+    validates :phonenumber__c, presence: true
+    validates :guests__c, presence: true
+    validates :username__c, presence: true
   self.table_name = 'ninetytenbookings.booking__c'
 end
