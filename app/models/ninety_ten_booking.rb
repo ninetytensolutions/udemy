d04f7af17eee4bd4d9c3690991292d32edd36fbf
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 14:09:13 +0000

    1

--- app/models/ninety_ten_booking.rb
+++ app/models/ninety_ten_booking.rb
@@ -1,12 +1,10 @@
 class NinetyTenBooking < ActiveRecord::Base
-    
-    validates :date, presence: true
-    validates :time, presence: true
-    validates :firstName, presence: true
-    validates :lastName, presence: true
-    validates :phoneNumber, presence: true
-    validates :noOfGuests, presence: true
-    validates :username, presence: true
-
+    #validates :date, presence: true
+    #validates :time, presence: true
+    #validates :firstName, presence: true
+    #validates :lastName, presence: true
+    #validates :phoneNumber, presence: true
+    #validates :noOfGuests, presence: true
+    #validates :username, presence: true
   self.table_name = 'ninetytenbookings.booking__c'
 end
