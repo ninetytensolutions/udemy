3e7f37b3d57878e2a92339386e6172b8a59982c8
Andrew Pereira <pereira.andrew@gmail.com>
2017-09-25 01:07:07 +0000

    Styled Form, Show and validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
        @article = Article.new(article_params)
        
         if @article.save
-            flash[:notice] = "Article successfully saved"
+            flash[:success] = "Article successfully saved"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
    def update
        
         if @article.update(article_params)
-            flash[:notice] = "Article successfully updated"
+            flash[:success] = "Article successfully updated"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
 
         @article.destroy
         
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
         
         redirect_to articles_path
         
